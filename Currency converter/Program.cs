fcfef3863281fb8907263c9dc6bd5cc096822957
Totli1 <nikto.81@internet.ru>
2025-05-27 21:11:43 +0300

    Добавление проекта

---
+++ Currency converter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Currency_converter
+{
+    internal class Program
+    {
+        static async  Task Main(string[] args)
+        {
+            Console.WriteLine("Введите первую валюту");
+            string first = Console.ReadLine();
+            Console.WriteLine("Введите вторую валюту");
+            string second = Console.ReadLine();
+            using var client = new HttpClient();
+            string message = await client.GetStringAsync($"https://v6.exchangerate-api.com/v6/0188f496c2b1a16ea17833c5/latest/{first}");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+            };
+            var rates = JsonSerializer.Deserialize<ExchangeRateResponse>(message, options);
+            if (rates.Conversion_rates.TryGetValue(second, out decimal usdRate))
+            {
+                Console.WriteLine($"1 {first} = {usdRate} {second}");
+            }
+
+        }
+       
+    }
+
+    public class ExchangeRateResponse
+    {
+        public Dictionary<string, decimal> Conversion_rates { get; set; }
+    }
+ 
+}
